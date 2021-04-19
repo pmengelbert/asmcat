732867b6aa9dd58ebdab89e2f4e3f2b5c96ddd05
Peter Engelbert <pmengelbert@gmail.com>
2021-04-18 20:23:16 -0500

    Add constants
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- asmcat.s
+++ asmcat.s
@@ -1,4 +1,9 @@
-MAX_READ_BYTES = 1 << 16
+BUF_SIZE = 1 << 16
+SYSCALL_READ = 0
+SYSCALL_WRITE = 1
+SYSCALL_OPEN = 2
+SYSCALL_CLOSE = 3
+SYSCALL_EXIT = 60
 .section .bss
 .lcomm buf, 1 << 16
 
@@ -6,29 +11,29 @@ MAX_READ_BYTES = 1 << 16
 .globl _start
 
 _start:
-	popq %rdi
+	xorl %r15d, %r15d # default to file descriptor 0 = stdin
+	popq %rdi # this is argc
 	decq %rdi
-	movl %edi, %r15d
-	jz read_and_write
+	jz read_and_write # if argc was 1, it will now be zero, in which case, read from stdin
 
-open_file:
-	movl $2, %eax
+open_file: # otherwise open the file and save the descriptor in %r15d
+	movl $SYSCALL_OPEN, %eax # syscall 2: open
 	movq 8(%rsp), %rdi
 	xorq %rsi, %rsi
 	syscall
 	movl %eax, %r15d
 
 read_and_write:
-	xorq %rax, %rax
+	movl $SYSCALL_READ, %eax
 	movl %r15d, %edi
 	movl $buf, %esi
-	movl $MAX_READ_BYTES, %edx
+	movl $BUF_SIZE, %edx
 	syscall
 	testl %eax, %eax
 	jz close_file
 	movl %eax, %edx
 
-	movl $1, %eax
+	movl $SYSCALL_WRITE, %eax
 	movl $1, %edi
 	syscall
 	testl %eax, %eax
@@ -39,11 +44,11 @@ close_file:
 	testl %r15d, %r15d
 	jz exit
 
-	movl $3, %eax
+	movl $SYSCALL_CLOSE, %eax
 	movl %r15d, %edi
 	syscall
 
 exit:
-	movl $60, %eax
+	movl $SYSCALL_EXIT, %eax
 	popq %rdi
 	syscall
